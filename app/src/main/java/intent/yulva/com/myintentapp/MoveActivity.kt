7f766292db1e19f6299367d74388806bbef0966a
mifta04 <ditata456@gmail.com>
2019-04-23 20:06:25 +0700

    tugas 4

---
+++ app/src/main/java/intent/yulva/com/myintentapp/MoveActivity.kt
@@ -0,0 +1,18 @@
+package intent.yulva.com.myintentapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebSettings
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+
+
+class MoveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
